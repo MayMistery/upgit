993b6d0e13e30ac3a1698368412bf7231f3aa841
maybemia <1754704389@qq.com>
2023-07-27 17:15:07 +0800

    v1.1

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -29,18 +31,56 @@ func updateRemoteURL(path, newURL string) error {
 	return cmd.Run()
 }
 
-func updateRepo(path string) error {
-	cmd := exec.Command("git", "pull")
+func getChangedFiles(path string) ([]string, error) {
+	cmd := exec.Command("git", "status", "--porcelain")
 	cmd.Dir = path
-	out, err := cmd.CombinedOutput()
+	out, err := cmd.Output()
 	if err != nil {
-		return fmt.Errorf("failed to pull repo %v: %v\n%s", path, err, out)
+		return nil, err
 	}
-	fmt.Printf("Updated repo at %s\n", path)
-	return nil
+
+	lines := strings.Split(strings.TrimSpace(string(out)), "\n")
+	var changedFiles []string
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		if len(parts) > 1 {
+			changedFiles = append(changedFiles, parts[1])
+		}
+	}
+	return changedFiles, nil
+}
+
+func resetAndPullRepo(path string) error {
+	cmd := exec.Command("git", "reset", "--hard", "origin/HEAD")
+	cmd.Dir = path
+	cmd.Run()
+
+	cmdClean := exec.Command("git", "clean", "-fd")
+	cmdClean.Dir = path
+	cmdClean.Run()
+
+	cmdPull := exec.Command("git", "pull")
+	cmdPull.Dir = path
+	return cmdPull.Run()
+}
+
+func correctURL(remoteURL string) string {
+	githubPrefix := "https://github.com/"
+	gitlabPrefix := "https://gitlab.com/"
+
+	if strings.Contains(remoteURL, githubPrefix) {
+		return remoteURL[strings.Index(remoteURL, githubPrefix):]
+	}
+	if strings.Contains(remoteURL, gitlabPrefix) {
+		return remoteURL[strings.Index(remoteURL, gitlabPrefix):]
+	}
+	return remoteURL
 }
 
 func main() {
+	excludeDirs := flag.String("x", "", "Comma-separated list of directories to exclude")
+	flag.Parse()
+
 	currentDir, err := os.Getwd()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error getting current directory: %v\n", err)
@@ -54,37 +94,63 @@ func main() {
 	}
 
 	const targetPrefix = "https://github.com/"
+	var failedRepos []string
+	reader := bufio.NewReader(os.Stdin)
 
 	for _, file := range files {
-		if file.IsDir() {
+		if file.IsDir() && !strings.Contains(*excludeDirs, file.Name()) {
 			repoPath := filepath.Join(currentDir, file.Name())
 			if isGitRepo(repoPath) {
-				// 获取远程URL并更新（如果需要）
+				changedFiles, _ := getChangedFiles(repoPath)
+				if len(changedFiles) > 0 {
+					fmt.Printf("Changed files in repo %s:\n", repoPath)
+					for _, f := range changedFiles {
+						fmt.Println("  ", f)
+					}
+				} else {
+					fmt.Printf("No changes in repo %s\n", repoPath)
+				}
+
 				remoteURL, err := getRemoteURL(repoPath)
 				if err != nil {
 					fmt.Fprintf(os.Stderr, "Error getting remote URL for repo %s: %v\n", repoPath, err)
 					continue
 				}
 
-				// 查找目标前缀，并剪切URL
-				if idx := strings.Index(remoteURL, targetPrefix); idx != -1 {
-					newURL := remoteURL[idx:]
-					if newURL != remoteURL {
-						err := updateRemoteURL(repoPath, newURL)
-						if err != nil {
-							fmt.Fprintf(os.Stderr, "Error updating remote URL for repo %s: %v\n", repoPath, err)
-							continue
-						}
-						fmt.Printf("Updated remote URL for repo at %s\n", repoPath)
+				correctedURL := correctURL(remoteURL)
+				if correctedURL != remoteURL {
+					err := updateRemoteURL(repoPath, correctedURL)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Error updating remote URL for repo %s: %v\n", repoPath, err)
+						continue
 					}
+					fmt.Printf("Updated remote URL for repo at %s\n", repoPath)
 				}
 
-				// 拉取更新
-				err = updateRepo(repoPath)
+				err = resetAndPullRepo(repoPath)
 				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error updating repo %s: %v\n", repoPath, err)
+					fmt.Fprintf(os.Stderr, "Error updating repo %s due to local changes: %v\n", repoPath, err)
+					fmt.Printf("Do you want to reset local changes and pull the latest updates for %s? (y/n): ", repoPath)
+					response, _ := reader.ReadString('\n')
+					response = strings.TrimSpace(response)
+
+					if response == "y" {
+						err = resetAndPullRepo(repoPath)
+						if err != nil {
+							fmt.Fprintf(os.Stderr, "Error after reset and pull for repo %s: %v\n", repoPath, err)
+						}
+					} else {
+						failedRepos = append(failedRepos, repoPath)
+					}
 				}
 			}
+		}
+	}
+
+	if len(failedRepos) > 0 {
+		fmt.Println("\nRepositories not updated due to local changes:")
+		for _, repo := range failedRepos {
+			fmt.Println(repo)
 		}
 	}
 }
